3d7fdca560fd69afec8fa72d96531a7f33c1a6d1
Luke <luketaft@gmail.com>
2022-06-21 17:43:07 +0100

    generate new "connect_4.py"
    deleted old "connect_4.py" as i changed development environment to
    spyder for this project. i dont think it makes any difference but it
    feels like crossing "t's" and dotting "i's"

--- connect_4.py
+++ connect_4.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 17:39:13 2022
+
+@author: okitraz
+"""
+
 playing_board=[[0,1,2,3,4,5],
                [0,1,2,3,4,5],
                [0,1,2,3,4,5],
@@ -10,4 +18,3 @@ playing_board=[[0,1,2,3,4,5],
 while True: 
   for item in playing_board:
     print(item)
-
