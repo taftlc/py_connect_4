38746b83cdc3f74377395943f37cf36d78eed033
Luke <luketaft@gmail.com>
2022-06-22 18:29:53 +0100

    divded section 1 into functions

--- connect_4.py
+++ connect_4.py
@@ -6,42 +6,48 @@
     Last Modified: 21, June 2022
 """
 
+
 #x = horizantal axis
 #y = vertical axis
      
+def make_pb():
+    playing_board=[[" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " ", " "]
+                   ] 
+    return playing_board
 
-playing_board=[[" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "],
-               [" ", " ", " ", " ", " ", " "]
-               ] 
+def print_pb(playing_board):
 
-#test pieces for the board
-playing_board[3][2] = "X"
-playing_board[5][5] = "O"
-
-
-#header containing orientation numbers
-header_nums = " "
-for num in range(0, len(playing_board)):
-    header_nums += "  "+ str(num) +"   "
-print(header_nums)
-print ("|-----" * (len(playing_board)) + "|")
-
-#printing rows
-for row in range(len(playing_board[0])):
-    print("|     " * (len(playing_board) +1))
+    #test pieces for the board
+    playing_board[0][1] = "X"
+    playing_board[5][5] = "O"
     
-    active_row = ""
-    for column in range(len(playing_board)):
-        active_row += ("|  "+str(playing_board[column][row])) + "  "
-    print(active_row + "| " + str(row))
-
-    print("|     " * (len(playing_board) +1))
     
+    #header containing orientation numbers
+    header_nums = " "
+    for num in range(0, len(playing_board)):
+        header_nums += "  "+ str(num) +"   "
+    print(header_nums)
     print ("|-----" * (len(playing_board)) + "|")
     
+    #printing rows
+    for row in range(len(playing_board[0])):
+        print("|     " * (len(playing_board) +1))
+        
+        active_row = ""
+        for column in range(len(playing_board)):
+            active_row += ("|  "+str(playing_board[column][row])) + "  "
+        print(active_row + "| " + str(row))
     
+        print("|     " * (len(playing_board) +1))
+        
+        print ("|-----" * (len(playing_board)) + "|")
+
+
+playing_board = make_pb()
+print_pb(playing_board)
